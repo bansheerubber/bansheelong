d6eb7421fbe7e505c78f0030ef9366172617c229
bansheerubber <gary.cactus.email@gmail.com>
2022-07-30 23:00:33 -0700

    add watcher

---
+++ watcher/src/main.rs
@@ -0,0 +1,42 @@
+use notify::{ Op, RawEvent, RecursiveMode, Watcher, raw_watcher };
+use std::sync::mpsc::channel;
+
+use bansheelong_types::{ IO, Resource, write_database };
+
+#[tokio::main]
+async fn main() {
+	let file = "/home/me/Projects/bansheetodo/todo-list";
+	let (tx, rx) = channel();
+	let mut watcher = raw_watcher(tx).unwrap();
+	watcher.watch("/home/me/Projects/bansheetodo", RecursiveMode::Recursive).unwrap();
+
+	let mut io = IO {
+		resource: Resource {
+			reference: String::from("http://localhost:3000")
+		},
+		..IO::default()
+	};
+
+	loop {
+		match rx.recv() {
+			Ok(RawEvent{ path: Some(path), op: Ok(op), cookie: _ }) => {
+				if path.to_str() == Some(file) && op == Op::CLOSE_WRITE {
+					println!("oh boy");
+					
+					if let Err(error)
+						= io.parse_from_human_readable(String::from(file))
+					{
+						println!("{:?}", error);
+						continue;
+					}
+
+					if let Err(error) = write_database(&io.database, None, io.resource.clone()).await {
+						println!("{:?}", error);
+					}
+				}
+			},
+			Ok(event) => println!("broken event: {:?}", event),
+			Err(e) => println!("watch error: {:?}", e),
+		};
+	}
+}
