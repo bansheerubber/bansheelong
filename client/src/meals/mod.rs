3bc00c7969b8ebc8131eef8d2484c8d9580e934a
bansheerubber <gary.cactus.email@gmail.com>
2022-08-18 14:36:03 -0700

    split out meals planner

--- client/src/meals/mod.rs
+++ client/src/meals/mod.rs
@@ -1,17 +1,20 @@
-pub(crate) mod image;
+pub(crate) mod image_utils;
+pub(crate) mod planner;
 pub(crate) mod render;
 pub(crate) mod right_panel;
 
+use std::collections::HashMap;
 use std::sync::Arc;
+use std::time::Instant;
 
 use bansheelong_types::{ Date, IO, PlannedMeal };
+use iced::{ button, image, scrollable };
 
-use crate::menu::Menu;
+use crate::menu::{ Menu, BUTTON_COUNT };
 
-pub(crate) use image::download_image;
-pub(crate) use image::has_image;
-pub(crate) use image::is_valid_image_url;
-pub(crate) use render::View;
+pub(crate) use image_utils::download_image;
+pub(crate) use image_utils::has_image;
+pub(crate) use image_utils::is_valid_image_url;
 
 #[derive(Debug, Clone)]
 pub enum Message {
@@ -33,8 +36,52 @@ pub enum Message {
 	Update(Option<Arc<IO>>),
 }
 
+#[derive(Debug)]
+struct PlannedInfo {
+	image: image::Handle,
+	image_state: image::viewer::State,
+	ingredient_button_states: Vec<button::State>,
+	ingredients_state: scrollable::State,
+	mapping: HashMap<Date, PlannedMeal>,
+	meal_button_states: Vec<button::State>,
+	meal_index: Option<Date>,
+	meals_position: f32,
+	meals_state: scrollable::State,
+	remove_meal_state: button::State,
+	switch_planner_state: button::State,
+}
+
+#[derive(Debug)]
+struct PlannerInfo {
+	day_button_states: Vec<button::State>,
+	day_index: Option<i8>,
+	image: image::Handle,
+	image_state: image::viewer::State,
+	ingredients_state: scrollable::State,
+	meal_add_state: button::State,
+	month_index: u32, // starts from 0
+	next_month_state: button::State,
+	previous_month_state: button::State,
+	recipe_button_states: Vec<button::State>,
+	recipe_index: Option<usize>,
+	recipes_position: f32,
+	recipes_state: scrollable::State,
+	state: PlannerState,
+	year_index: u8, // starts at 0, 0 represents 2000
+}
+
 #[derive(Clone, Copy, Debug)]
 pub(crate) enum PlannerState {
 	DaySelect,
 	MealSelect,
+}
+
+#[derive(Debug)]
+pub(crate) struct View {
+	button_states: [button::State; BUTTON_COUNT as usize],
+	database: Option<Arc<IO>>,
+	last_interaction: Option<Instant>,
+	planned: PlannedInfo,
+	planner: PlannerInfo,
+	showing_planner: bool,
 }
--- client/src/meals/render.rs
+++ client/src/meals/render.rs
@@ -1,13 +1,12 @@
 use std::collections::HashMap;
-use std::sync::Arc;
 use std::time::{ Duration, Instant };
 
-use bansheelong_types::{ Date, IO, PlannedMeal };
+use bansheelong_types::Date;
 use chrono::{ Datelike, Local, NaiveDate };
 use iced::{ Button, Column, Command, Container, Element, Length, Row, Scrollable, Space, Text, alignment, button, image, scrollable };
 
 use crate::constants;
-use crate::meals::{ Message, PlannerState, has_image, right_panel };
+use crate::meals::{ Message, PlannedInfo, PlannerInfo, PlannerState, View, has_image };
 use crate::menu::{ Menu, BUTTONS, BUTTON_AREA_SIZE, BUTTON_COUNT, BUTTON_HEIGHT, BUTTON_SPACING };
 use crate::style;
 
@@ -32,50 +31,6 @@ fn get_current_month() -> u32 {
 
 fn get_current_year() -> u8 {
 	(Local::now().year() - 2000) as u8
-}
-
-#[derive(Debug)]
-struct PlannerInfo {
-	day_button_states: Vec<button::State>,
-	day_index: Option<i8>,
-	image: image::Handle,
-	image_state: image::viewer::State,
-	ingredients_state: scrollable::State,
-	meal_add_state: button::State,
-	month_index: u32, // starts from 0
-	next_month_state: button::State,
-	previous_month_state: button::State,
-	recipe_button_states: Vec<button::State>,
-	recipe_index: Option<usize>,
-	recipes_position: f32,
-	recipes_state: scrollable::State,
-	state: PlannerState,
-	year_index: u8, // starts at 0, 0 represents 2000
-}
-
-#[derive(Debug)]
-struct PlannedInfo {
-	image: image::Handle,
-	image_state: image::viewer::State,
-	ingredient_button_states: Vec<button::State>,
-	ingredients_state: scrollable::State,
-	mapping: HashMap<Date, PlannedMeal>,
-	meal_button_states: Vec<button::State>,
-	meal_index: Option<Date>,
-	meals_position: f32,
-	meals_state: scrollable::State,
-	remove_meal_state: button::State,
-	switch_planner_state: button::State,
-}
-
-#[derive(Debug)]
-pub struct View {
-	button_states: [button::State; BUTTON_COUNT as usize],
-	database: Option<Arc<IO>>,
-	last_interaction: Option<Instant>,
-	planned: PlannedInfo,
-	planner: PlannerInfo,
-	showing_planner: bool,
 }
 
 impl View {
@@ -188,135 +143,6 @@ impl View {
 		for _ in 0..weeks * 7 {
 			self.planner.day_button_states.push(button::State::new());
 		}
-	}
-
-	fn get_meal_planner(&mut self) -> Row<Message> {
-		let day_buttons = self.planner.day_button_states.iter_mut();
-
-		let selected_date = if let Some(day) = self.planner.day_index {
-			Some(Date {
-				day: day as u8,
-				month: self.planner.month_index as u8 + 1,
-				year: 22,
-			})
-		} else {
-			None
-		};
-
-		let (right_panel, remaining_width) = right_panel::get_planner_right_panel(
-			self.planner.state,
-			self.planner.year_index,
-			self.planner.month_index,
-			day_buttons,
-			&mut self.planner.previous_month_state,
-			&mut self.planner.next_month_state,
-			&mut self.planner.meal_add_state,
-			&mut self.planner.ingredients_state,
-			self.planner.recipe_index,
-			selected_date,
-			&self.planner.image,
-			&mut self.planner.image_state,
-			self.database.as_ref().unwrap().clone()
-		);
-
-		// meal list
-		let mut scrollable = Scrollable::new(&mut self.planner.recipes_state)
-		.width(Length::Units(remaining_width))
-		.height(Length::Fill)
-		.padding([20, 15, 20, 0])
-		.style(style::TodoScrollable)
-		.on_scroll_absolute(move |offset| Message::RecipesScroll(offset))
-		.min_height(((BUTTON_AREA_SIZE + BUTTON_HEIGHT + BUTTON_SPACING) + constants::WINDOW_HEIGHT) as u32)
-		.push( // add menu navigation
-			self.button_states
-			.iter_mut()
-			.zip(BUTTONS.iter())
-			.fold(
-				Column::new()
-					.spacing(BUTTON_SPACING)
-					.padding([0, 0, 20, 0]),
-				|button_column, (state, (name, menu_type))| {
-					if menu_type != &Menu::Meals {
-						button_column.push(
-							Button::new(
-								state,
-								Text::new(name.clone())
-									.width(Length::Fill)
-									.horizontal_alignment(alignment::Horizontal::Center)
-							)
-								.style(style::TodoMenuButton)
-								.width(Length::Fill)
-								.height(Length::Units(BUTTON_HEIGHT))
-								.on_press(Message::MenuChange(menu_type.clone()))
-						)
-					} else {
-						button_column
-					}
-				}
-			)
-			.push(
-				Button::new(
-					&mut self.planned.switch_planner_state,
-					Text::new("Planned meals")
-						.width(Length::Fill)
-						.horizontal_alignment(alignment::Horizontal::Center)
-				)
-					.style(style::SpecialMenuButton)
-					.width(Length::Fill)
-					.height(Length::Units(BUTTON_HEIGHT))
-					.on_press(Message::SwitchToPlanned)
-			)
-		);
-
-		// construct recipes list
-		scrollable = self.database.as_ref().unwrap().meals_database.recipes.iter()
-			.zip(self.planner.recipe_button_states.iter_mut())
-			.enumerate()
-			.fold(scrollable, |scrollable, (index, (recipe, button_state))| {
-				scrollable.push(
-					Button::new(
-						button_state,
-						Container::new(
-							Row::new()
-								.width(Length::Fill)
-								.push(
-									Text::new(recipe.name.clone())
-										.width(Length::Fill)
-								)
-								.push(
-									if let Some(minutes) = recipe.minutes {
-										Text::new(format!(
-											"{} {}{}",
-											minutes,
-											if let PlannerState::MealSelect = self.planner.state {
-												"min"
-											} else {
-												"minute"
-											},
-											if minutes != 1 {
-												"s"
-											} else {
-												""
-											}
-										))
-									} else {
-										Text::new("")
-									}
-								)
-						)
-							.width(Length::Fill)
-							.padding(10)
-					)
-						.on_press(Message::PlannerRecipeSelect(index as usize))
-						.style(style::DarkButton)
-						.padding(0)
-				)
-				.push(Space::new(Length::Units(0), Length::Units(10)))
-			});
-
-		Row::new()
-			.push(scrollable)
-			.push(right_panel)
 	}
 
 	fn get_meal_planned(&mut self) -> Row<Message> {
