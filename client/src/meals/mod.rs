d973ec4aaa0f3200d27b9f06e23df8fd5a1ca505
bansheerubber <gary.cactus.email@gmail.com>
2022-08-18 14:29:40 -0700

    split out meals right panel

--- client/src/meals/mod.rs
+++ client/src/meals/mod.rs
@@ -1,8 +1,40 @@
 pub(crate) mod image;
 pub(crate) mod render;
+pub(crate) mod right_panel;
+
+use std::sync::Arc;
+
+use bansheelong_types::{ Date, IO, PlannedMeal };
+
+use crate::menu::Menu;
 
 pub(crate) use image::download_image;
 pub(crate) use image::has_image;
 pub(crate) use image::is_valid_image_url;
-pub(crate) use render::Message;
 pub(crate) use render::View;
+
+#[derive(Debug, Clone)]
+pub enum Message {
+	APIAddPlannedMeal(PlannedMeal),
+	APIRemovePlannedMeal(Date),
+	APIUpdatePlannedMeal(PlannedMeal),
+	MenuChange(Menu),
+	PlannedIngredientsScroll,
+	PlannedMealsScroll(f32),
+	PlannedMealSelect(Date),
+	PlannerDaySelect(i8),
+	PlannerMonthSelect(u8, u32),
+	PlannerRecipeScroll,
+	PlannerRecipeSelect(usize),
+	RecipesScroll(f32),
+	SwitchToPlanned,
+	SwitchToPlanner,
+	Tick,
+	Update(Option<Arc<IO>>),
+}
+
+#[derive(Clone, Copy, Debug)]
+pub(crate) enum PlannerState {
+	DaySelect,
+	MealSelect,
+}
